c45920b7f1a17816d3e00f6be5ffcb189419fe5c
mehranGulzar <m.gulzar@web.de>
2022-07-04 15:32:27 +0200

    Inizial commit Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import model.Student;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+
+        Student s1 = new Student("alex",32, "Hamburg");
+    }
+}
